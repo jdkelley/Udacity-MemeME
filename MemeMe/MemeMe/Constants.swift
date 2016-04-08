15533108b8241282fa81fd541bed59792efe3011
= <=>
2016-04-08 09:53:05 -0400

    - Add magic Strings constants file. - UI: Add textfields - UI: Fix constraints

---
+++ MemeMe/MemeMe/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  MemeMe
+//
+//  Created by Joshua Kelley on 4/8/16.
+//  Copyright © 2016 Joshua Kelley. All rights reserved.
+//
+
+import Foundation
+
+struct DefaultText {
+    
+    static let TopTextFieldText = "TOP"
+    
+    static let BottomTextFieldText = "BOTTOM"
+    
+}
